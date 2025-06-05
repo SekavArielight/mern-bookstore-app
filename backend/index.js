7a495edeb944d91cd580c5ffb7baadb02d6bc1eb
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-05 16:38:51 +0100

    Refactor response handling in get all books route to use json() and add missing error handling for get book by ID route

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.get('/books', async (request, response) => {
   try {
     const books = await Book.find({});
 
-    return response.status(200).send({
+    return response.status(200).json({
       count: books.length,
       data: books,
     });
@@ -58,6 +58,18 @@ app.get('/books', async (request, response) => {
 });
 
 // Route to get a book by ID
+app.get('/books/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
