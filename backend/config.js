2620968e2a8096504341a73214bb2df1aedb915f
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-05 16:48:08 +0100

    Move sensitive configuration to .env file and update imports in index.js

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 
-'mongodb+srv://godswillapya:code234@cluster0.yb44t.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
