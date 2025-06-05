c1f3a6394a7d3a71722b38e5bf2edc6cfc55aefa
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-05 14:09:53 +0100

    Refactor index.js to comment out unused code and routes

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,37 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+// import mongoose from "mongoose";
+// import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send('Welcome to MERN Stack Tutorial')
+app.listen(PORT, () => {
+  console.log(`App is listening to port: ${PORT}`);
 })
 
-// Route to save a new book
-app.post('/books', async (request, response) => {
-    try {
-        
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
+// app.get('/', (request, response) => {
+//     console.log(request)
+//     return response.status(234).send('Welcome to MERN Stack Tutorial')
+// })
 
-mongoose
-  .connect(mongoDBURL)
-  .then(() => {
-    console.log('App connected to database');
-    app.listen(PORT, () => {
-        console.log(`App is listening to port: ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// // Route to save a new book
+// app.post('/books', async (request, response) => {
+//     try {
+        
+//     } catch (error) {
+//         console.log(error.message);
+//         response.status(500).send({message: error.message});
+//     }
+// });
+
+// mongoose
+//   .connect(mongoDBURL)
+//   .then(() => {
+//     console.log('App connected to database');
+//     app.listen(PORT, () => {
+//         console.log(`App is listening to port: ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
