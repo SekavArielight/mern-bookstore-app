84a1e546115072d22e98501e7a238612da0dbe05
Godswill Sekav Apya <godswillapya@gmail.com>
2025-08-13 11:43:10 +0100

    Update imports in index.js to use config.js and add config.js to .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT, mongoDBURL } from "./.env";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 
