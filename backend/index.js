94ed2abaa7ca4ff00a3521697d1b4bb9c0254c31
Godswill Sekav Apya <godswillapya@gmail.com>
2025-08-14 14:41:29 +0100

    Add route to delete a book with error handling

--- backend/index.js
+++ backend/index.js
@@ -97,6 +97,24 @@ app.put("/books/:id", async (request, response) => {
   }
 });
 
+// Route to delete a book
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      response.status(404).send({ message: "Book not found" });
+    }
+
+    return response.status(204).send({message: "Book deleted successfully"});
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({message: error.messsage});
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
